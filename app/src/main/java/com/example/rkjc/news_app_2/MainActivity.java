1f94110ca148013a2380a8b13ae3eb185607b22b
Smitkumar Patel <30848598+smitkumarpatel4@users.noreply.github.com>
2018-11-08 11:56:07 -0800

    Initial commit

---
+++ app/src/main/java/com/example/rkjc/news_app_2/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.rkjc.news_app_2;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+    private EditText mSearchText;
+    private ProgressBar mProgressBar;
+
+    private static final String TAG = "MainActivity";
+    private static final int LOADER_ID=1;
+    private static final String SEARCH_QUERY_URL_EXTRA = "searchQuery";
+    private static final String SEARCH_QUERY_RESULT = "searchResults";
+    private String newsSearchResult;
+
+    private RecyclerView mRecyclerView;
+    private NewsRecyclerViewAdapter mAdapter;
+    private ArrayList<NewsItem> newsItem = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSearchText = (EditText) findViewById(R.id.search_box);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.news_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new NewsRecyclerViewAdapter(MainActivity.this, newsItem);
+        mRecyclerView.setAdapter(mAdapter);
+        //makeNewsURL();
+
+        if(savedInstanceState != null && savedInstanceState.containsKey(SEARCH_QUERY_RESULT)){
+            String searchResults = savedInstanceState.getString(SEARCH_QUERY_RESULT);
+            populateRecyclerView(searchResults);
+        }
+
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(SEARCH_QUERY_RESULT, newsSearchResult);
+
+    }
+
+    private URL  makeNewsURL(){
+        URL newsURL = NetworkUtils.buildUrl();
+        return newsURL;
+        //new NewsQueryTask().execute(newsURL);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            URL url = makeNewsURL();
+            Bundle bundle = new Bundle();
+            bundle.putString(SEARCH_QUERY_URL_EXTRA, url.toString());
+            LoaderManager loaderManager = getSupportLoaderManager();
+            Loader<String> gitHubSearchLoader = loaderManager.getLoader(LOADER_ID);
+            if(gitHubSearchLoader == null){
+                loaderManager.initLoader(LOADER_ID, bundle, this).forceLoad();
+            }else{
+                loaderManager.restartLoader(LOADER_ID, bundle, this).forceLoad();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public android.support.v4.content.Loader<String> onCreateLoader(int id, @Nullable final Bundle bundle) {
+        return new AsyncTaskLoader<String>(this)
+        {
+            @Override
+            protected void onStartLoading() {
+                super.onStartLoading();
+                Log.d(TAG, "onStartLoading called");
+                super.onStartLoading();
+                if(bundle == null){
+                    Log.d(TAG, "bundle null");
+                    return;
+                }
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Nullable
+            @Override
+            public String loadInBackground() {
+                Log.d(TAG, "loadInBackground called");
+
+                String newsSearchQuery = bundle.getString(SEARCH_QUERY_URL_EXTRA);
+                if(newsSearchQuery == null || newsSearchQuery.isEmpty()){
+                    return null;
+                }
+                try {
+                    Log.d(TAG, "begin network call");
+                    newsSearchResult = NetworkUtils.getResponseFromHttpUrl(new URL(newsSearchQuery));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Log.d(TAG, newsSearchResult);
+                return newsSearchResult;
+            }
+        };
+    }
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        Log.d("mycode", data);
+        mProgressBar.setVisibility(View.GONE);
+        populateRecyclerView(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+
+    public void populateRecyclerView(String searchResults ){
+        Log.d("mycode", searchResults);
+        newsItem = JsonUtils.parseNews(searchResults);
+        mAdapter.mNewsList.addAll(newsItem);
+        mAdapter.notifyDataSetChanged();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/rkjc/news_app_2/NewsRecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package com.example.rkjc.news_app_2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class NewsRecyclerViewAdapter  extends RecyclerView.Adapter<NewsRecyclerViewAdapter.NewsViewHolder> {
+    Context mContext;
+    public ArrayList<NewsItem>  mNewsList;
+
+    public NewsRecyclerViewAdapter(Context context, ArrayList<NewsItem> newsList) {
+        this.mContext = context;
+        this.mNewsList = newsList;
+    }
+
+
+    @Override
+    public NewsViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(R.layout.news_item, viewGroup, shouldAttachToParentImmediately);
+        NewsViewHolder newsViewHolder = new NewsViewHolder(view);
+        return newsViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(NewsViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNewsList.size();
+    }
+
+    public class NewsViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView description;
+        TextView date;
+
+        public NewsViewHolder(View itemView){
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
+            // url = (TextView) itemView.findViewById(R.id.url);
+            date = (TextView) itemView.findViewById(R.id.date);
+        }
+
+        public void bind(final int listIndex){
+            title.setText("Title:"  + mNewsList.get(listIndex).getTitle());
+            description.setText("Description:" +mNewsList.get(listIndex).getDescription());
+            // url.setText(mNewsList.get(listIndex).getUrl());
+            date.setText("Date:"+mNewsList.get(listIndex).getPublishedAt());
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    String urlString = mNewsList.get(listIndex).getUrl();
+//                    Intent intent = new Intent(mContext, WebActivity.class);
+//                    intent.putExtra(Intent.EXTRA_TEXT, urlString);
+//                    mContext.startActivity(intent);
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(urlString));
+                    mContext.startActivity(intent);
+                }
+            });
+        }
+    }
+}
